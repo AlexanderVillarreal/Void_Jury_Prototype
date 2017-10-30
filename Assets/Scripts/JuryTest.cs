47cf3720c0ed5a10037130135f344802d3145c4b
AlexanderVillarreal <villarrealaw@gmail.com>
2017-10-30 19:43:12 -0400

    First Commit

---
+++ Assets/Scripts/JuryTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JuryTest : MonoBehaviour {
+
+	public void Judgement() {
+		Renderer rend = GetComponent<Renderer> ();
+
+		float roll = Random.Range (0.0f, 10.0f);
+
+		print ("They rolled a " + roll);
+
+		if (roll <= 6) {
+			rend.material.color = Color.green;
+		} else {
+			rend.material.color = Color.red;
+		}
+	}
+
+	void OnMouseDown() {
+		Judgement ();
+	}
+}
